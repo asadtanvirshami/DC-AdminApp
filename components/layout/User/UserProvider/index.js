4dd3838592ebb392cffd9f3e840bd492962a9749
Asad Tanvir Shami <91515695+asadtanvirshami@users.noreply.github.com>
2023-11-20 08:27:35 -0800

    minor bug resolved

--- components/layout/User/UserProvider/index.js
+++ components/layout/User/UserProvider/index.js
@@ -7,7 +7,7 @@ export function useUser() {
   return useContext(UserContext);
 }
 
-export const UserProvider = memo(({ children }) => {
+ const UserProvider = memo(({ children }) => {
   const [userData, setUserData] = useState(() => {
     const userString = Cookies.get("user");
     if (userString) {
